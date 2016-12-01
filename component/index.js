ee928c2e3a31748933a2b01dd677a21a47971436
Grant Nestor <grantnestor@gmail.com>
2016-12-01 07:29:42 -0800

    Combine labextension and nbextension into single Python package

--- component/index.js
+++ component/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Plotly from 'plotly.js';
 
 export default class Component extends React.Component {
 
+  componentDidMount() {
+    const { data, layout } = this.getFigure();
+    Plotly.newPlot(this.el, data, layout);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return this.props.data !== nextProps.data;
+  }
+
+  componentDidUpdate() {
+    const { data, layout } = this.getFigure();
+    this.el.data = data;
+    this.el.layout = layout;
+    Plotly.redraw(this.el);
+  }
+
   render() {
-      return (
-        <div>
-          {JSON.stringify(this.props.data)}
-        </div>
-      );
+    const { layout } = this.getFigure();
+    const style = {};
+    if (layout && layout.height && !layout.autosize) style.height = layout.height;
+    return (
+      <div style={style} ref={(el) => { 
+        this.el = el; 
+      }} />
+    );
+  }
+  
+  getFigure = () => {
+    const { data } = this.props;
+    if (typeof data === 'string') return JSON.parse(data);
+    return data;
   }
 
 }
